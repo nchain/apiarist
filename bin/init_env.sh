e48a43ebdfcf5928c4af4da526e0d6f430235f25
tigerich <tigerich@163.com>
2021-06-19 21:09:38 +0800

    update clef port

--- bin/init_env.sh
+++ bin/init_env.sh
@@ -2,3 +2,8 @@ sudo apt-get install -y python3-pip
 sudo pip3 install -y virtualenv 
 
 pip install PyYAML
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+docker network create bee_hive
