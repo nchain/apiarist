991b209b3b405e8cfff7f3150911b4983dc3f4b4
tigerich <tigerich@163.com>
2021-06-20 12:48:29 +0800

    add docker install sh

--- bin/init_env.sh
+++ bin/init_env.sh
@@ -7,9 +7,3 @@ sudo pip3 install virtualenv
 pip install PyYAML
 
 docker network ls |grep bee_hive || docker network create bee_hive
-
-# install docker-compose
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo mv ./bin/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
---
+++ bin/install_docker.sh
@@ -0,0 +1,15 @@
+sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg     lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker ps -a
+sudo usermod -aG docker rad
+
+
+# install docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo mv ./bin/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
