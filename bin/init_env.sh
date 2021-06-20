d0e71e1af39df91b8c05843e51089fb1348c45a3
tigerich <tigerich@163.com>
2021-06-20 10:36:27 +0800

    add docker-compose

--- bin/init_env.sh
+++ bin/init_env.sh
@@ -11,5 +11,5 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-
 
 docker network create bee_hive
 
-mv ./bin/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo mv ./bin/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
