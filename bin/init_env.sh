de08950505a7893f75282187aa58efe901bdd9bb
tigerich <tigerich@163.com>
2021-06-20 10:34:33 +0800

    add docker-compose

--- bin/init_env.sh
+++ bin/init_env.sh
@@ -10,3 +10,6 @@ pip install PyYAML
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 docker network create bee_hive
+
+mv ./bin/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
